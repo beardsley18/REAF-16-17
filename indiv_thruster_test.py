0aa59eb64c0c6ef59589f45e6587ac185561684b
nuc1 <tamu.reaf2017@gmail.com>
2017-07-26 11:06:47 -0500

    New stuff from pool videos

--- indiv_thruster_test.py
+++ indiv_thruster_test.py
@@ -5,69 +5,69 @@ ser = serial.Serial('COM3', 9600)
 time.sleep(2)
 
 try:
-    ser.write('CUS'.encode('utf-8'))  #change thruster each time
+    ser.write('SV0'.encode('utf-8'))  #change thruster each time
     ser.flush()
     # print('test')
     abc = ser.read(3).decode('utf-8')
     print(abc)
-    time.sleep(5)
+    time.sleep(2)
     ser.flush()
     
-    # ser.write('SM0'.encode('utf-8'))  #change thruster each time
-    # ser.flush()
-    # # print('test')
-    # abc = ser.read(3).decode('utf-8')
-    # print(abc)
-    # time.sleep(2)
-    # ser.flush()
+    ser.write('SM0'.encode('utf-8'))  #change thruster each time
+    ser.flush()
+    # print('test')
+    abc = ser.read(3).decode('utf-8')
+    print(abc)
+    time.sleep(2)
+    ser.flush()
     
-    # ser.write('SDA'.encode('utf-8'))  #change thruster each time
-    # ser.flush()
-    # # print('test')
-    # abc = ser.read(3).decode('utf-8')
-    # print(abc)
-    # time.sleep(2)
-    # ser.flush()
+    ser.write('SDA'.encode('utf-8'))  #change thruster each time
+    ser.flush()
+    # print('test')
+    abc = ser.read(3).decode('utf-8')
+    print(abc)
+    time.sleep(2)
+    ser.flush()
     
-    # ser.write('SDF'.encode('utf-8'))  #change thruster each time
-    # ser.flush()
-    # # print('test')
-    # abc = ser.read(3).decode('utf-8')
-    # print(abc)
-    # time.sleep(2)
-    # ser.flush()
+    ser.write('SDF'.encode('utf-8'))  #change thruster each time
+    ser.flush()
+    # print('test')
+    abc = ser.read(3).decode('utf-8')
+    print(abc)
+    time.sleep(2)
+    ser.flush()
     
-    # ser.write('PV0'.encode('utf-8'))  #change thruster each time
-    # ser.flush()
-    # # print('test')
-    # abc = ser.read(3).decode('utf-8')
-    # print(abc)
-    # time.sleep(2)
-    # ser.flush()
+    ser.write('PV0'.encode('utf-8'))  #change thruster each time
+    ser.flush()
+    # print('test')
+    abc = ser.read(3).decode('utf-8')
+    print(abc)
+    time.sleep(2)
+    ser.flush()
     
-    # ser.write('PM0'.encode('utf-8'))  #change thruster each time
-    # ser.flush()
-    # # print('test')
-    # abc = ser.read(3).decode('utf-8')
-    # print(abc)
-    # time.sleep(2)
-    # ser.flush()
+    ser.write('PM0'.encode('utf-8'))  #change thruster each time
+    ser.flush()
+    # print('test')
+    abc = ser.read(3).decode('utf-8')
+    print(abc)
+    time.sleep(2)
+    ser.flush()
     
-    # ser.write('PDA'.encode('utf-8'))  #change thruster each time
-    # ser.flush()
-    # # print('test')
-    # abc = ser.read(3).decode('utf-8')
-    # print(abc)
-    # time.sleep(2)
-    # ser.flush()
+    ser.write('PDA'.encode('utf-8'))  #change thruster each time
+    ser.flush()
+    # print('test')
+    abc = ser.read(3).decode('utf-8')
+    print(abc)
+    time.sleep(2)
+    ser.flush()
     
-    # ser.write('PDF'.encode('utf-8'))  #change thruster each time
-    # ser.flush()
-    # # print('test')
-    # abc = ser.read(3).decode('utf-8')
-    # print(abc)
-    # time.sleep(2)
-    # ser.flush()
+    ser.write('PDF'.encode('utf-8'))  #change thruster each time
+    ser.flush()
+    # print('test')
+    abc = ser.read(3).decode('utf-8')
+    print(abc)
+    time.sleep(2)
+    ser.flush()
     
     ser.write('STO'.encode('utf-8')) #change thruster each time
     ser.flush()
